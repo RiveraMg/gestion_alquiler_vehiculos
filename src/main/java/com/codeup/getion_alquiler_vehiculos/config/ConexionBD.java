d28889387f145eede1e5a5bf94467275752500fd
RiveraMg <riveramelanie720@gmail.com>
2025-10-01 19:14:52 -0500

    actualizacion carpetas completas

---
+++ src/main/java/com/codeup/getion_alquiler_vehiculos/config/ConexionBD.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.codeup.getion_alquiler_vehiculos.config;
+
+/**
+ *
+ * @author Coder
+ */
+public class ConexionBD {
+    
+}
